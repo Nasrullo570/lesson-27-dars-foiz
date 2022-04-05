211cf69c805bee3bf5b496cd4c24fa74f52daa7b
nasrullo <nasrulloorinboyev1997@gmail.com>
2022-04-06 00:03:56 +0500

    foiz

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function number(foiz) {
+    let number = {
+      Needs: 0.5 * foiz,
+      Wants: 0.3 * foiz,
+      Savings: 0.2 * foiz,
+    };
+    return number;
+  };
+  console.log(number(10000));
+  console.log(number(50000));
+  console.log(number(13450));
